fa79027192ff6c4d005d4844ec31f96af309058c
luocheng2015 <444790866@qq.com>
2017-07-10 14:23:22 +0800

    flySearch项目初始化

---
+++ src/com/fly/search/main/FlySearchMain.java
@@ -0,0 +1,12 @@
+package com.fly.search.main;
+
+/**
+ * 功能描述
+ * 创建人 luocheng
+ * 创建时间2017/7/10.
+ */
+public class FlySearchMain {
+    public static void main(String[] args) {
+        System.out.println("hello，项目初始化");
+    }
+}
